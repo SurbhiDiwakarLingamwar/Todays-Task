6889256c9629bde2e38a19dde666c52f6c9cd6f2
SurbhiDiwakarLingamwar <60814737+SurbhiDiwakarLingamwar@users.noreply.github.com>
2020-02-27 12:56:16 +0530

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace BOOKAPP
+{
+    public class Book
+    {
+        [Required]
+        [Key]
+    
+        public int Bid { get; set; }
+        public string Btitle { get; set; }
+        [Display(Name ="Book Title")]
+        [Required]
+        public string Category{ get; set; }
+        [DataType(DataType.Currency)]
+        public double price { get; set; }
+        public string AuthorName { get; set; }
+        [Display(Name = "Author Name")]
+        [Required]
+        public string publisher{ get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Released Date")]
+        [Required]
+        public DateTime Releaseddate { get; set; }
+
+
+
+
+
+
+    }
+}
